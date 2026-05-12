2029fd044f4787064e2f8e10da3dcdd954d3da53
diegolopes-iesb <diego.lopes@iesb.edu.br>
2026-05-12 10:58:50 -0300

    Update teste.bat

--- teste.bat
+++ teste.bat
@@ -1,3 +1,7 @@
+@echo off
+
+echo. > cafeteiras.log
+
 call cafeteira01.bat 1
 call cafeteira02.bat 2
 call cafeteira03.bat 3
@@ -20,3 +24,5 @@ call cafeteira03.bat 3
 call cafeteira04.bat 2
 
 type cafeteiras.log
+
+pause
