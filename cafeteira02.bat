5ecdb299f1b37dde8903ed260aa4604283f3f8b4
diegolopes-iesb <diego.lopes@iesb.edu.br>
2026-05-13 00:31:22 -0300

    Update cafeteira02.bat

--- cafeteira02.bat
+++ cafeteira02.bat
@@ -2,11 +2,8 @@
 
 set op=%1
 
-if "%op%"=="1"
-    node cafeteria.js 02 p
+if "%op%"=="1" node cafeteria.js 02 p
 
-if "%op%"=="2"
-    node cafeteria.js 02 m
+if "%op%"=="2" node cafeteria.js 02 m
 
-if "%op%"=="3"
-    node cafeteria.js 02 g
+if "%op%"=="3" node cafeteria.js 02 g
