d9a1e5dc855a9714f9ada59116c675ebeea6d247
diegolopes-iesb <diego.lopes@iesb.edu.br>
2026-05-12 23:10:01 -0300

    Update cafeteira01.bat

--- cafeteira01.bat
+++ cafeteira01.bat
@@ -1,18 +1,12 @@
 @echo off
-echo =====================
-echo      CAFETEIRA 01
-echo =====================
-echo.
-echo [1] Pequeno
-echo [2] Medio
-echo [3] Grande
-echo.
 
-set /p op=Escolha:
+set op=%1
 
 if "%op%"=="1"
     node cafeteria.js 01 p
+
 if "%op%"=="2"
     node cafeteria.js 01 m
-if "%op%"=="3" 
-	node cafeteria.js 01 g
+
+if "%op%"=="3"
+    node cafeteria.js 01 g
